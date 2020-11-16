d26ea34a097a7aacce756bbe803540fcfd6e21b9
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2020-11-16 17:09:23 +0800

    Initial commit

---
+++ 2D_Midterm_107051206/Assets/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("2D");
+    }
+
+    public void QuitGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+        
+    }
+
+    public void Back()
+    {
+        print("返回選單");
+        SceneManager.LoadScene("選單");
+
+    }
+ 
+}
