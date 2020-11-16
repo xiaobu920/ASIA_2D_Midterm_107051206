8bbe4f9a1e50cad520d2c74dbda53104c4fadd41
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2020-11-17 01:51:42 +0800

    增到關卡管理器
    17凌晨

--- 2D_Midterm_107051206/Assets/LevelManager.cs
+++ 2D_Midterm_107051206/Assets/LevelManager.cs
@@ -3,20 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-    // (參數1，參數2，...，參數N)
-    // 類型 名稱
     public void NextLevel(string nameLv)
     {
         SceneManager.LoadScene(nameLv);
     }
 
-    public void BackToMenu()
-    {
-        SceneManager.LoadScene("選單");
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
-    }
+    
 }
